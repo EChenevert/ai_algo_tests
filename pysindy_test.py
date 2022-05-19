11ad340c605c351f4d617fb4d8ae5e6ee6fd78d0
Chenevert, Etienne A <etachen@iu.edu>
2022-05-19 16:49:59 -0400

    attempted different types of sindy implements

--- pysindy_test.py
+++ pysindy_test.py
@@ -7,14 +7,23 @@ import numpy as np
 #                                                     'Verified Pin Height (mm)',
 #                                                     'Average Accretion (mm)',
 #                                                     'Surface Elevation Change Rate']]
-df = pd.read_csv(r"D:\Etienne\crmsDATATables\Small_bysite_byyear_season_noZeroesAccSurf.csv",
-                        encoding='unicode escape')[['Verified Pin Height (mm)',
-                                                    'Average Accretion (mm)']]
-# Will use the verified pin hieght for this exploratory analysis
 
-df = df.dropna()
-df = df.drop(1, axis=0)
-time = np.asarray(df.index)
+
+# df = pd.read_csv(r"D:\Etienne\crmsDATATables\Small_bysite_byyear_season_noZeroesAccSurf.csv",
+#                         encoding='unicode escape')[['Verified Pin Height (mm)',
+#                                                     'Average Accretion (mm)']]
+# # Will use the verified pin hieght for this exploratory analysis
+#
+# df = df.dropna()
+# df = df.drop(1, axis=0)
+# time = np.asarray(df.index)
+
+# Jacks Qriv and Qwave data ===============================================================
+
+df_div = pd.read_excel(r"D:\Etienne\qs\ForEti.xlsx", sheet_name="Flux Divided By Channel")
+df_raw = pd.read_excel(r"D:\Etienne\qs\ForEti.xlsx", sheet_name="Measured at Mouth")
+
+timeQs = np.arange(len(df_div))
 
 # # Base model library implementation ===================================================
 # import pysindy as ps
@@ -28,26 +37,26 @@ time = np.asarray(df.index)
 # modelBase.print()
 # ensemble_coefs = modelBase.coef_list
 
-# # Deep time polynomial implementation =====================================================================
-#
-# from sklearn.preprocessing import PolynomialFeatures
-# from deeptime.sindy import STLSQ
-# from deeptime.sindy import SINDy
-#
-# library = PolynomialFeatures(degree=3)
-#
-# optimizer = STLSQ(threshold=0.2)
-# # Instantiate the estimator
-# estimator = SINDy(
-#     library=library,
-#     optimizer=optimizer,
-#     input_features=["Elevation", "Accretion"]  # The feature names are just for printing
-# )
-#
-# # Fit the estimator to data
-# estimator.fit(np.asarray(df), t=time)
-# modelDPoly = estimator.fetch_model()
-# modelDPoly.print()
+# Deep time polynomial implementation =====================================================================
+
+from sklearn.preprocessing import PolynomialFeatures
+from deeptime.sindy import STLSQ
+from deeptime.sindy import SINDy
+
+library = PolynomialFeatures(degree=2)
+
+optimizer = STLSQ(threshold=0.002, alpha=0.5, normalize=True)
+# Instantiate the estimator
+estimator = SINDy(
+    library=library,
+    optimizer=optimizer,
+    input_features=["Qw", "Qr"]  # The feature names are just for printing
+)
+
+# Fit the estimator to data
+estimator.fit(np.asarray(df_div), t=timeQs)
+modelDPoly = estimator.fetch_model()
+modelDPoly.print()
 
 # # Trigonometric Library Implementation ============================================
 #
@@ -58,36 +67,39 @@ time = np.asarray(df.index)
 # Fourier_library = ps.FourierLibrary()
 # # from pysindy.feature_library import PDELibrary
 #
-# modelFour = ps.SINDy(feature_names=['Elevation', 'Accretion'], optimizer=optimizer, feature_library=Fourier_library)
+# modelFour = ps.SINDy(feature_names=['Qw', 'Qr'], optimizer=optimizer, feature_library=Fourier_library)
 # print(modelFour)
 #
-# modelFour.fit(np.asarray(df), t=time)
+# modelFour.fit(np.asarray(df_div), t=timeQs)
 # modelFour.print()
 
 # Implementation of the Generalized Library =====================================
 # Instantiate and fit the SINDy model
-import pysindy as ps
-modelGen = ps.SINDy()
-modelGen.fit(np.asarray(df), t=time)
-modelGen.print()
-
+#
+# timeQs = np.arange(len(df_div))
+#
+# import pysindy as ps
+# modelGen = ps.SINDy()
+# modelGen.fit(np.asarray(df_div), t=timeQs)
+# modelGen.print()
+#
 
 # ============ Simulate from inital conditions ================================
 
-x0 = 437.05714  # elevation
-y0 = 45.083333  # Accretion
+x0 = 0.5 # df_div["Qw"][0]  # elevation
+y0 = 0.5 # df_div["Qr - Flux divided by channel method"][0]  # Accretion
 
-sim = modelGen.simulate([x0, y0], t=time)
+sim = modelDPoly.simulate([x0, y0], t=timeQs)
 
 # plot
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 ax.plot(x0, y0, "ro", label="Initial condition", alpha=0.6, markersize=8)
-ax.plot(np.asarray(df['Verified Pin Height (mm)']), np.asarray(df['Average Accretion (mm)']),
+ax.plot(np.asarray(df_div["Qw"]), np.asarray(df_div["Qr - Flux divided by channel method"]),
         "b", label="Exact solution", alpha=0.4, linewidth=4)
 ax.plot(sim[:, 0], sim[:, 1], "k--", label="SINDy model", linewidth=3)
-ax.set(xlabel="Elevation", ylabel="Accretion")
+ax.set(xlabel="Qw", ylabel="Qr_divided")
 ax.legend()
 plt.show()
 
