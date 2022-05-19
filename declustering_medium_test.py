a690f9bb48c95efc15acd33a08950f1c3def69cf
Chenevert, Etienne A <etachen@iu.edu>
2022-05-19 09:58:18 -0400

    got declustering going

--- declustering_medium_test.py
+++ declustering_medium_test.py
@@ -19,13 +19,26 @@ plt.show()
 #                                  cmin=1, cmax=15)
 
 W, Csize, Dmean = geostat.declus(df, 'Longitude', 'Latitude', 'Average Accretion (mm)',
-                                 cmin=1, cmax=20, iminmax=0, noff=5, ncell=100)
+                                 cmin=1, cmax=20, iminmax=1, noff=5, ncell=80)
 
 # W: is the output weights. Weights for each point to reduce the bias
 # Csize is the output cell sizes. grid
 # Dmean is the declustered means for each cell size
 
+# plot to visualize the minum declustered mean for accretion
 sns.lineplot(x=Csize, y=Dmean)
 plt.ylabel("Declustered mean Average Accretion (mm)")
 plt.xlabel("Cell Size (lat long units..?)")
+plt.show()
+
+# plot to visualize the wieght values spatially
+sns.scatterplot(x=df["Longitude"], y=df["Latitude"], hue=W)
+plt.show()
+
+# Comparing naive to declustered statistics
+sns.histplot(df['Average Accretion (mm)'])
+plt.title("Naive Accretion")
+plt.show()
+sns.histplot(df['Average Accretion (mm)']*W)
+plt.title("Declustered Accretion")
 plt.show()
